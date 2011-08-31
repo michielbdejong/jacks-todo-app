53975e9cd2c8c25d838a118bdb35015512ce686c
Michiel@unhosted <michiel@unhosted.org>
2011-08-31 17:30:58 +0200

    first commit

---
+++ www/unhosted/config.js
@@ -0,0 +1,14 @@
+var appBaseUrl = 'http://todo.dev.unhosted.org';
+//var installationType = 'flower';
+
+var config = {
+	appUrl: appBaseUrl + '/',
+	doUrl: appBaseUrl + '/unhosted/do.php',
+	loginUrl: appBaseUrl + '/unhosted/login.html',
+	//registerUrl: appBaseUrl + '/unhosted/register.html',
+	callbackUrl: appBaseUrl + '/unhosted/callback.html',
+	importCallbackUrl: appBaseUrl + '/unhosted/import.html',
+	clientId: 'todo.dev.unhosted.org',
+	dataScope: 'jacktodoapp',
+	homeDomain: 'todo.dev.unhosted.org'
+}
---
+++ www/unhosted/config.php
@@ -0,0 +1,9 @@
+<?php
+class UnhostedSettings {
+	const installationType = 'flower';
+	const protocol = 'https';
+	const domain = 'todo.dev.unhosted.org';
+	const davDir = '/var/www/todo/dav/';
+	const pwdDir = '/var/www/todo/pwd/';
+	const walletDir = '/var/www/todo/wallet/';
+}
